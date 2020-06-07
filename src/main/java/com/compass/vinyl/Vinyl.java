efa1eb5f7cd0ee354b033486e425f1d5e562c953
Vivek Viswanathan <vivek.viswanathan@compass.com>
2020-06-07 14:49:08 +0530

    Added jacoco for coverage

--- src/main/java/com/compass/vinyl/Vinyl.java
+++ src/main/java/com/compass/vinyl/Vinyl.java
@@ -3,12 +3,19 @@
 package com.compass.vinyl;
 
 import com.compass.vinyl.player.RecordPlayer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- *
+ * Vinyl represents the layer to record & playback the scenarios.
+ * Mode - determines if the current usage if of recording or playback
+ * RecordingConfig - determines the (de)serialization for data
+ * Player - determines where the data is stored & retrieved
  *
  */
 public class Vinyl {
+
+    private static Logger LOG = LoggerFactory.getLogger(Vinyl.class);
 
     private Mode mode;
 
@@ -19,7 +26,34 @@ public class Vinyl {
     private Vinyl(){
     }
 
-    public static class Build {
+    public Scenario intercept(Scenario scenario) {
+        if (mode.equals(Mode.RECORD))
+            record(scenario);
+        else if (mode.equals(Mode.REPLAY))
+            return playback(scenario);
+        return scenario;
+    }
+
+    public void record(Scenario scenario) {
+        try {
+            boolean status = player.record(scenario, config);
+            if (!status)
+                LOG.warn("Recording failed for the scenario:" + scenario
+                        + " and config: " + config);
+        }
+        catch(RuntimeException r) {
+            LOG.error("Recording failed for the scenario:" + scenario
+            + " and config: " + config, r);
+        }
+    }
+
+    public Scenario playback(Scenario scenario) {
+        return player.playback(scenario, config);
+    }
+
+    public static class Builder {
+
+        private RecordPlayer player;
 
         private RecordingConfig config;
 
@@ -29,32 +63,23 @@ public class Vinyl {
             Vinyl vinyl = new Vinyl();
             vinyl.mode = this.mode;
             vinyl.config = this.config;
+            vinyl.player = this.player;
             return vinyl;
         }
 
-        public Build withRecordingConfig(RecordingConfig config) {
+        public Builder usingRecordingConfig(RecordingConfig config) {
             this.config = config;
             return this;
         }
 
-        public Build withMode(Mode mode) {
+        public Builder usingMode(Mode mode) {
             this.mode = mode;
             return this;
         }
-    }
 
-    public void record(Scenario scenario) {
-        try {
-            boolean status = player.record(scenario, config);
+        public Builder withPlayer(RecordPlayer player) {
+            this.player = player;
+            return this;
         }
-        catch(RuntimeException r) {
-            //Remove Sysout
-            System.out.println("Error occurred while recording. Error = " + r.getMessage());
-        }
-
-    }
-
-    public Scenario playback(Scenario scenario) {
-        return player.playback(scenario, config);
     }
 }
---
+++ src/test/java/com/compass/vinyl/VinylTest.java
@@ -0,0 +1,83 @@
+package com.compass.vinyl;
+
+import com.compass.vinyl.player.LocalFileSystemRecordPlayer;
+import com.compass.vinyl.player.LocalFileSystemRecordPlayerTest;
+import com.compass.vinyl.player.RecordPlayer;
+import com.compass.vinyl.serializer.JSONSerializer;
+import com.compass.vinyl.serializer.models.Animal;
+import com.compass.vinyl.serializer.models.Bird;
+import com.compass.vinyl.serializer.models.Lion;
+import com.compass.vinyl.serializer.models.Tiger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class VinylTest {
+
+    private static String recordingPath;
+
+    private Vinyl vinyl;
+
+    RecordPlayer player;
+
+    Scenario expectedScenario;
+
+    RecordingConfig config;
+
+    @BeforeClass
+    public static void pathSetup() {
+        try {
+            Path temp = Files.createTempDirectory("vinyl-");
+            recordingPath = temp.toAbsolutePath().toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setup() {
+        player = new LocalFileSystemRecordPlayer();
+        config = new RecordingConfig(JSONSerializer.getInstance(), recordingPath);
+
+        List<Bird> birds = new ArrayList<>();
+        birds.add(new Bird("Parrot"));
+        birds.add(new Bird("Peacock"));
+
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Lion("Alex", 23, Arrays.asList("Orange", "Yellow")));
+        animals.add(new Tiger("Cat", 23, Arrays.asList("Yellow", "Black")));
+
+        expectedScenario = new Scenario(LocalFileSystemRecordPlayerTest.class.getCanonicalName(),
+                "test",
+                Arrays.asList(new Data("birds", birds)),
+                new Data("animals", animals));
+    }
+
+    @Test
+    public void record() {
+        vinyl = new Vinyl.Builder().usingMode(Mode.RECORD).withPlayer(player).usingRecordingConfig(config).create();
+        vinyl.record(expectedScenario);
+    }
+
+    @Test
+    public void playback() {
+        vinyl = new Vinyl.Builder().usingMode(Mode.REPLAY).withPlayer(player).usingRecordingConfig(config).create();
+        Scenario scenario = vinyl.playback(expectedScenario);
+
+        List<Animal> animals = (List<Animal>) scenario.getOutput().getValue();
+        List<Animal> expectedAnimals = (List<Animal>) expectedScenario.getOutput().getValue();
+        for (int i = 0; i < expectedAnimals.size(); i++) {
+            Assert.assertTrue("Replay of the scenario failed.", animals.get(i).equals(expectedAnimals.get(i)));
+        }
+    }
+}
